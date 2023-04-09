578362b886f373e8d55fa40ab48dcdc0dc7a0410
Rajat Sharma <sharma.raja@northeastern.edu>
2023-04-09 15:48:45 -0400

    Add tooling

---
+++ webpack-configs/webpack.common.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = () => {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules)/,
+          use: ['babel-loader']
+
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(jpg|png)$/,
+          use: {
+            loader: 'url-loader',
+          },
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Weather Forecast',
+        template: 'webpack-configs/templates/index.html',
+      }),
+    ],
+  };
+};
